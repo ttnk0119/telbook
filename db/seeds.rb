57dc14442b67af1aac643547c10b09808016d5f3
nt <ne290059@senshu-u.jp>
2018-11-10 19:40:07 +0900

    Modify: 全体/seeds.rbのデバッグ用の出力を削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
 table_names = %w(friends)
 table_names.each do |table_name|
   path =Rails.root.join("db/seeds", Rails.env, table_name + ".rb")
-  puts "filename #{path}"
   if File.exist?(path)
     puts "Creating #(table_name)..."
     require path
