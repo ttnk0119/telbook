291a8d9d10afbbbaaf16ea70c45b4f35c696602c
nt <ne290059@senshu-u.jp>
2018-11-10 20:29:23 +0900

    Add: 全体/telephoneのシードデータを作成

---
+++ db/seeds/development/telephones.rb
@@ -0,0 +1,10 @@
+Friend.ids.each do |fid|
+  friend = Friend.find(fid)
+  0.upto(3) do |idx|
+    telephone = Telephone.new(
+      number: "080-#{rand(1000..9999)}-#{rand(1000..9999)}",
+      cellphone: 0.equal?(idx % 2)
+    )
+    friend.telephones << telephone 
+  end
+end
