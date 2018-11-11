5ae0311a4af0bd45ef4207809f42dff2710d6fe6
nt <ne290059@senshu-u.jp>
2018-11-11 23:11:54 +0900

    Add: 全体/バリデーションを追加

--- app/models/friend.rb
+++ app/models/friend.rb
@@ -1,3 +1,6 @@
 class Friend < ActiveRecord::Base
   has_many :telephones, dependent: :destroy
+
+  validates :name, presence: true,
+    length: { maximum: 10, allow_blank: true }
 end
--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -1,3 +1,8 @@
 class Telephone < ActiveRecord::Base
   belongs_to :friend
+
+  validates :number, presence: true,
+    format: { with: /\A[-()0-9]+\Z/ },
+    length: { maximum: 15 },
+    uniqueness: true
 end
