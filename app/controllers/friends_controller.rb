910676ba7e989102488d6070c49f2c2909a75f68
nt <ne290059@senshu-u.jp>
2018-11-12 13:02:59 +0900

    Add: 友達一覧、友達新規登録・編集/friendコントローラにcreate,updata,destroyを記述

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -18,11 +18,32 @@ class FriendsController < ApplicationController
   end
 
   def create
+    @friend = Friend.new(friend_params)
+    if @friend.save
+      redirect_to friends_path, notice: "友達を登録しました。"
+    else
+      render "new"
+    end
   end
 
   def update
+    @friend = Friend.find(params[:id])
+    @friend.assign_attributes(friend_params)
+    if @friend.save
+      redirect_to friends_path, notice: "友達の情報を更新しました。"
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @friend = Friend.find(params[:id])
+    @friend.destroy
+    redirect_to :friends, notice: "友達を削除しました。"
+  end
+
+  private
+  def friend_params
+    params.require(:friend).permit(:name)
   end
 end
