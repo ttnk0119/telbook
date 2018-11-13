e1c7c22567e2a1eb380785b16a2280d378eb867e
nt <ne290059@senshu-u.jp>
2018-11-13 14:28:25 +0900

    Modify: 全体/仮データの量を増やす
    ページネーションの動作を確認するためにfriendとtelephoneのデータ数を増やした

--- db/seeds/development/friends.rb
+++ db/seeds/development/friends.rb
@@ -1,7 +1,7 @@
-fname = ["佐藤", "鈴木", "高橋", "田中"]
-gnames = ["太郎", "次郎", "花子"]
-0.upto(9) do |idx|
+fname = ["佐藤", "鈴木", "高橋", "田中", "山田"]
+gnames = ["太郎", "次郎", "花子", "雛子", "ジョン", "メアリー"]
+0.upto(25) do |idx|
   Friend.create(
-    name: "#{fname[idx % 4]} #{gnames[idx % 3]}"
+    name: "#{fname[idx % 5]} #{gnames[idx % 6]}"
   )
 end
--- db/seeds/development/telephones.rb
+++ db/seeds/development/telephones.rb
@@ -1,6 +1,6 @@
 Friend.ids.each do |fid|
   friend = Friend.find(fid)
-  0.upto(3) do |idx|
+  0.upto(7) do |idx|
     telephone = Telephone.new(
       number: "080-#{rand(1000..9999)}-#{rand(1000..9999)}",
       cellphone: 0.equal?(idx % 2)
