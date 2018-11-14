514fc05c027c824ee871bfe79ed2f15b213246e0
nt <ne290059@senshu-u.jp>
2018-11-15 02:28:31 +0900

    Add: 全体/cssを追加(1)

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,7 +1,7 @@
 class FriendsController < ApplicationController
   def index
     @friends = Friend.order("id")
-      .paginate(page: params[:page], per_page: 10)
+      .paginate(page: params[:page], per_page: 5)
   end
 
   def show
