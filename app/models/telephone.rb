69a660cdd8f5d6f9138d0fa25ecedf8ef34f328d
nt <ne290059@senshu-u.jp>
2019-06-29 13:27:09 +0900

    Add: 友達詳細/checkboxの文を追加

--- app/models/telephone.rb
+++ app/models/telephone.rb
@@ -2,7 +2,7 @@ class Telephone < ActiveRecord::Base
   belongs_to :friend
 
   validates :number, presence: true,
-    format: { with: /\A[-()0-9]+\Z/ },
+    format: { with: /\A[-()0-9]+\z/ },
     length: { maximum: 15 },
     uniqueness: true
 end
